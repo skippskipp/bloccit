f830d5ce5c2bc98face34a4c75fcbde8e8c4e24d
skippskipp <Scott@Melanies-MacBook-Pro.local>
2017-06-10 07:47:29 -0400

    checkpoint21 complete

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
 
   resources :advertisements
 
-  resources :posts
+  resources :topics do
+    resources :posts, except: [:index]
+  end
+
 
   get 'about' => 'welcome#about'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,14 @@
 require 'random_data'
 
+# Create Topics
+15.times do
+  Topic.create!(
+  name: RandomData.random_sentence,
+  description: RandomData.random_paragraph
+  )
+end
+topics = Topic.all
+
 #Create Ads
 50.times do
   Advertisement.create!(
@@ -23,6 +32,7 @@ questions = Question.all
 #Create Posts
 50.times do
   Post.create!(
+  topic: topics.sample,
   title: RandomData.random_sentence,
   body: RandomData.random_paragraph
   )
@@ -45,6 +55,7 @@ end
 Comment.find_or_create_by!(body: 'Eleven has been and will be waiting', post: mypost)
 
 puts "Seed finished"
+puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
 puts "#{Advertisement.count} ads created"
