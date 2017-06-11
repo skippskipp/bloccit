d720cf8533a136e1f6cf1e2e751f90e1eae5f669
skippskipp <Scott@Melanies-MacBook-Pro.local>
2017-06-11 00:14:01 -0400

    assignment 21 complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
 
+  get 'sponsored_posts/show'
+
+  get 'sponsored_posts/new'
+
+  get 'sponsored_posts/edit'
+
   resources :questions
 
   resources :advertisements
 
   resources :topics do
     resources :posts, except: [:index]
+    resources :sponsored_posts, except: [:index]
   end
 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -39,6 +39,17 @@ questions = Question.all
 end
 posts = Post.all
 
+#Create Sponsored Posts
+50.times do
+  SponsoredPost.create!(
+  topic: topics.sample,
+  title: RandomData.random_sentence,
+  body: RandomData.random_paragraph,
+  price: RandomData.random_integer
+  )
+end
+sponsored_posts = SponsoredPost.all
+
 # Create Comments
 100.times do
   Comment.create!(
@@ -57,6 +68,7 @@ Comment.find_or_create_by!(body: 'Eleven has been and will be waiting', post: my
 puts "Seed finished"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
+puts "#{SponsoredPost.count} sponsored posts created"
 puts "#{Comment.count} comments created"
 puts "#{Advertisement.count} ads created"
 puts "#{Question.count} questions created"
