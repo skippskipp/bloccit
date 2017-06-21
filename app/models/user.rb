04f652190fe7c8a5199f21818d3816c385766265
skippskipp <Scott@Melanies-MacBook-Pro.local>
2017-06-20 22:13:43 -0400

    Auth prep

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ActiveRecord::Base
 
   before_save { self.email = email.downcase if email.present? }
+  before_save { self.name = name.split(/ /)
+                name.each do |name| name.capitalize
+                end
+                name.join(" ")
+              }
+
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
   validates :password, length: { minimum: 6 }, allow_blank: true
