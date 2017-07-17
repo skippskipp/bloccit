7a0b6fa972244b8a5007c6eda38e7ad1e7055e27
skippskipp <Scott@Melanies-MacBook-Pro.local>
2017-07-17 16:21:47 -0400

    cp30 complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -42,12 +42,14 @@ questions = Question.all
 
 #Create Posts
 50.times do
-  Post.create!(
-  user: users.sample,
-  topic: topics.sample,
-  title: RandomData.random_sentence,
-  body: RandomData.random_paragraph
+  post = Post.create!(
+    user: users.sample,
+    topic: topics.sample,
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph
   )
+  post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+  rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
 end
 posts = Post.all
 
@@ -100,5 +102,6 @@ puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{SponsoredPost.count} sponsored posts created"
 puts "#{Comment.count} comments created"
+puts "#{Vote.count} votes created"
 puts "#{Advertisement.count} ads created"
 puts "#{Question.count} questions created"
