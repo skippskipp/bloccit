f80f0ed29e80c37d04da6074df8949e87d4cbebf
skippskipp <skipp.Contractor@rti.org>
2017-07-24 16:41:25 -0400

    assignment 31 complete

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -12,4 +12,13 @@ class FavoriteMailer < ApplicationMailer
 
     mail(to: user.email, cc: "rajeev.singh@bloc.io", subject: "New comment on #{post.title}")
   end
+
+  def new_post(post)
+    headers["Message-ID"] = "<post/#{post.id}@safe-depths-99149.herokuapp>"
+    headers["In-Reply-To"] = "<post/#{post.id}@safe-depths-99149.herokuapp>"
+    headers["References"] = "<post/#{post.id}@safe-depths-99149.herokuapp>"
+
+    @post = post
+    mail(to: post.user.email, subject: "You are now following #{post.title}")
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :topic
   belongs_to :user
-  after_create :create_vote
+  after_create :create_vote, :auto_favorite
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
   has_many :favorites, dependent: :destroy
@@ -37,6 +37,11 @@ class Post < ActiveRecord::Base
   private
   def create_vote
     user.votes.create!(value: 1, post: self, user: user)
+  end
+
+  def auto_favorite
+    Favorite.create!(post: self, user: self.user)
+    FavoriteMailer.new_post(self).deliver_now
   end
 
 end
